b2f8ba9c21cc73a5533fe5b89604bbbce08ae288
Zayar Myo Thwin <zayarmyothwin@gmail.com>
2021-06-22 00:22:09 +0630

    added code files

---
+++ code/array/array1.py
@@ -0,0 +1,3 @@
+list = [129, 283, 873, 125, 229, 23]
+for i in range(len(list)):
+    print(list[i])
---
+++ code/array/arraysum.py
@@ -0,0 +1,5 @@
+list = [1, 2, 3, 11, 32, 41, 55, 10]
+x = 0
+for i in list:
+    x = x+i
+print("Total:", x)
